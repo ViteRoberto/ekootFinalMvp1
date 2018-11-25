b23884811a29ec51e72d12d6daf3a12e0b465c4c
ViteRoberto <roberto.vite@hotmail.com>
2018-11-24 19:46:16 -0600

    Avance Home-ACS-Empresas SIN TABS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'acs', loadChildren: './acs/acs.module#AcsPageModule' },
+  { path: 'empresas', loadChildren: './empresas/empresas.module#EmpresasPageModule' },
 ];
 
 @NgModule({
